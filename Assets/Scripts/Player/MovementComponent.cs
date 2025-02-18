765a46d1d8cd196b6f437988c6d7a7e44f189240
CalviceGaming <diogo.pereira.d24@gmail.com>
2025-02-18 15:35:44 +0000

    Wallrunning :D

--- Assets/Scripts/Player/MovementComponent.cs
+++ Assets/Scripts/Player/MovementComponent.cs
@@ -20,6 +20,9 @@ public class MovementComponent : MonoBehaviour
     
     private bool collidingWithWall = false;
     private GameObject wallCollidingWith;
+    private GameObject lastWall;
+    private Vector3 wallNormal;
+    private bool wallRunning = false;
     
     [SerializeField] private List<GameObject> collectiblesList = new List<GameObject>();
     
@@ -32,11 +35,17 @@ public class MovementComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        playerSpeed = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        
+        Checkifgrounded();
+            
         PlayerMovement();
         
         CrouchAndSlide();
+
+        WallRunning();
         
-        if (Input.GetButton("Jump") && availableJump && grounded && !crouching && !sliding)
+        if (Input.GetButtonDown("Jump") && availableJump && grounded && !crouching && !sliding)
         {
             rb.AddForce(Vector3.up * 80, ForceMode.Impulse);
             availableJump = false;
@@ -45,29 +54,12 @@ public class MovementComponent : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag == "Ground")
-        {
-            availableJump = true;
-            grounded = true;
-        }
-        if (other.gameObject.tag == "Wall")
-        {
-            collidingWithWall = true;
-            wallCollidingWith = other.gameObject;
-        }
+
     }
 
     private void OnCollisionExit(Collision other)
     {
-        if (other.gameObject.tag == "Ground")
-        {
-            grounded = false;
-        }
-        if (other.gameObject.tag == "Wall")
-        {
-            collidingWithWall = false;
-            wallCollidingWith = null;
-        }
+
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -110,8 +102,7 @@ public class MovementComponent : MonoBehaviour
         {
             sideways = 0;
         }
-
-        if (!sliding)
+        if (!sliding && grounded)
         {
             Vector3 movementDirection = (transform.right * sideways + transform.forward * forward).normalized;
 
@@ -131,16 +122,78 @@ public class MovementComponent : MonoBehaviour
                 rb.AddForce(direction * acceleration / 2, ForceMode.Force);
             }
         }
+        else if (!grounded && !wallRunning)
+        {
+            Vector3 movementDirection = (transform.right * sideways + transform.forward * forward).normalized;
+
+            rb.AddForce(movementDirection * (0.3f * acceleration) , ForceMode.Force);
+
+            Vector3 flatVelocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+            playerSpeed = flatVelocity;
+            if (flatVelocity.magnitude > maxSpeed)
+            {
+                flatVelocity = flatVelocity.normalized * maxSpeed;
+                rb.velocity = new Vector3(flatVelocity.x, rb.velocity.y, flatVelocity.z);
+            }
+
+            if (flatVelocity.magnitude > 0 && forward == 0 && sideways == 0 && grounded)
+            {
+                Vector3 direction = -flatVelocity.normalized;
+                rb.AddForce(direction * acceleration / 2, ForceMode.Force);
+            }
+        }
     }
 
     private void WallRunning()
     {
-        if (collidingWithWall)
+        CheckifWall();
+        if (collidingWithWall && !grounded)
         {
-            if (Input.GetButton("Jump"))
+            if (Input.GetButtonDown("Jump") && playerSpeed.magnitude > maxSpeed/2)
             {
-                
+                if (lastWall != wallCollidingWith)
+                {
+                    lastWall = wallCollidingWith;
+                    wallRunning = true;
+                    rb.useGravity = false;
+                    rb.velocity = new Vector3(0, 0, 0);
+                    rb.AddForce(Vector3.up * 30, ForceMode.Impulse);
+                }
             }
+            else if (Input.GetButtonUp("Jump"))
+            {
+                if (wallRunning)
+                {
+                    rb.AddForce(Camera.main.transform.forward * 140, ForceMode.Impulse);
+                    wallRunning = false;
+                    rb.useGravity = true;
+                }
+            }
+
+            if (wallRunning)
+            {
+                Vector3 directionToGo = Vector3.Cross(wallNormal, Vector3.up).normalized;
+                if (Vector3.Dot(directionToGo, transform.forward) < 0)
+                {
+                    directionToGo = -directionToGo;
+                }
+                rb.AddForce(directionToGo * acceleration, ForceMode.Force);
+                
+                rb.AddForce(-wallNormal * 10, ForceMode.Force);
+                
+                rb.AddForce(Vector3.up * -10, ForceMode.Force);
+                if (playerSpeed.magnitude > maxSpeed)
+                {
+                    playerSpeed = playerSpeed.normalized * maxSpeed;
+                    rb.velocity = new Vector3(playerSpeed.x, rb.velocity.y, playerSpeed.z);
+                }
+            }
+            
+        }
+        else
+        {
+            wallRunning = false;
+            rb.useGravity = true;
         }
     }
 
@@ -175,6 +228,96 @@ public class MovementComponent : MonoBehaviour
             crouching = false;
             sliding = false;
             maxSpeed = 13f;
+        }
+        if (playerSpeed.magnitude <= 1 && sliding)
+        {
+            sliding = false;
+            crouching = true;
+            maxSpeed = 2f;
+        }
+    }
+
+    private void Checkifgrounded()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, gameObject.transform.lossyScale.y / 2 + 1f))
+        {
+            Debug.DrawRay(transform.position, Vector3.down * hit.distance, Color.yellow); 
+            //if (hit.collider.CompareTag("Ground"))
+            //{
+                availableJump = true;
+                grounded = true;
+                lastWall = null;
+                //}
+                //else
+                //{
+                //    grounded = false;
+                //}
+        }
+        else
+        {
+            grounded = false;
+        }
+    }
+
+    private void CheckifWall()
+    {
+        RaycastHit hitright;
+        RaycastHit hitleft;
+        RaycastHit hitbehing;
+        if (Physics.Raycast(transform.position, transform.right, out hitright, gameObject.transform.lossyScale.x / 2 + 1f))
+        {
+            Debug.DrawRay(transform.position, transform.right * hitright.distance, Color.yellow);
+            if (hitright.collider.gameObject.tag == "Wall")
+            {
+                wallCollidingWith = hitright.collider.gameObject;
+                collidingWithWall = true;
+                wallNormal = hitright.normal;
+            }
+        }
+        if (Physics.Raycast(transform.position, -transform.right, out hitleft, gameObject.transform.lossyScale.x / 2 + 1f))
+        {
+            Debug.DrawRay(transform.position, -transform.right * hitleft.distance, Color.yellow); 
+            if (hitleft.collider.gameObject.tag == "Wall")
+            {
+                wallCollidingWith = hitleft.collider.gameObject;
+                collidingWithWall = true;
+            }
+        }
+
+        if (wallRunning)
+        {
+            if (Physics.Raycast(transform.position, -transform.forward, out hitbehing, gameObject.transform.lossyScale.x / 2 + 1f))
+            {
+                Debug.DrawRay(transform.position, -transform.forward * hitbehing.distance, Color.yellow); 
+                if (hitbehing.collider.gameObject.tag == "Wall")
+                {
+                    wallCollidingWith = hitbehing.collider.gameObject;
+                    collidingWithWall = true;
+                }
+            }
+            if (Physics.Raycast(transform.position, -transform.right, out hitleft,gameObject.transform.lossyScale.x / 2 + 1f) 
+                || Physics.Raycast(transform.position, transform.right, out hitright, gameObject.transform.lossyScale.x / 2 + 1f)
+                || Physics.Raycast(transform.position, -transform.forward, out hitbehing, gameObject.transform.lossyScale.x / 2 + 1f))
+            {
+            
+            }
+            else
+            {
+                collidingWithWall = false;   
+            }   
+        }
+        else
+        {
+            if (Physics.Raycast(transform.position, -transform.right, out hitleft,gameObject.transform.lossyScale.x / 2 + 1f) 
+                || Physics.Raycast(transform.position, transform.right, out hitright, gameObject.transform.lossyScale.x / 2 + 1f))
+            {
+            
+            }
+            else
+            {
+                collidingWithWall = false;   
+            }   
         }
     }
 }
