1e62d1f77cb64ca3d703ed495f16badf2ee70cab
Tomás Monteiro <152517065+tomasotaldomonteiro@users.noreply.github.com>
2025-04-07 18:24:38 +0100

    JumpPads and other fixes for stuff

---
+++ Assets/JumpPad.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class JumpPad : MonoBehaviour
+{
+    private float bounceforce = 200f;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(Vector2.up * bounceforce, ForceMode.Impulse);
+        }
+    }
+    
+}
