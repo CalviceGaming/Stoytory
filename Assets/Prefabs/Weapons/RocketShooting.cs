b8db65098aecb5dd88a8bc0ecd92cc12fb61370f
Tomás Monteiro <152517065+tomasotaldomonteiro@users.noreply.github.com>
2025-04-23 16:19:30 +0100

    Losts of fixes, rockets complete but might  have bugs, explosion effect, and trail

---
+++ Assets/Prefabs/Weapons/RocketShooting.cs
@@ -0,0 +1,126 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class RocketShooting : MonoBehaviour
+{
+    [SerializeField] private GameObject rocket;           
+    [SerializeField] private GameObject rocketSpawn;      
+    [SerializeField] private GameObject rocketsParent;
+    private float shootTimer = 3f;                            
+    public UnityEvent onShootRocket;                         
+    [SerializeField] private float maxMagazine = 2;         
+    private float currentMagazine;                          
+    public UnityEvent onReloadRocket;                         
+    //private bool reloadingAnimation = false;       mesma merda aqui           
+    [SerializeField] private Text magazineText;               
+    
+    // Inputs
+    [SerializeField] private InputActionReference shootingAction;
+    [SerializeField] private bool shooting;
+    [SerializeField] private InputActionReference reloadAction;
+    [SerializeField] private bool reloading;
+
+    private void OnEnable()
+    {
+        magazineText.GetComponent<Text>().text = $"{currentMagazine}/{maxMagazine}";
+        shootingAction.action.Enable();
+        reloadAction.action.Enable();
+
+        shootingAction.action.started += OnShootingStarted;
+        reloadAction.action.started += OnReloadingStarted;
+    }
+    
+    private void OnDisable()
+    {
+        shootingAction.action.started -= OnShootingStarted;
+        reloadAction.action.started -= OnReloadingStarted;
+    }
+
+    private void Start()
+    {
+        currentMagazine = maxMagazine;
+        GetComponent<WeaponPosition>().endReload.AddListener(EndReload);
+        magazineText.GetComponent<Text>().text = $"{currentMagazine}/{maxMagazine}";
+    }
+    
+    private void OnShootingStarted(InputAction.CallbackContext callbackContext)
+    {
+        shooting = true;
+    }
+
+    private void OnReloadingStarted(InputAction.CallbackContext callbackContext)
+    {
+        reloading = true;
+    }
+
+    private void Update()
+    {
+        ShootingRocket();
+        Reload();
+    }
+
+    private void ShootingRocket()
+    {
+        
+        if (currentMagazine <= 0)
+        {
+            shooting = false;
+           // reloading = true; para dar uncoment quando tiver animaçao
+            return;
+        }
+        if (shooting && shootTimer > 0.1f && !reloading) 
+        {
+            shootTimer = 0f;
+            currentMagazine--;
+            magazineText.GetComponent<Text>().text = $"{currentMagazine}/{maxMagazine}";
+            GameObject rocketInstance = Instantiate(rocket, rocketSpawn.transform.position, rocketSpawn.transform.rotation, rocketsParent.transform);
+            rocketInstance.SetActive(true);
+            rocketInstance.GetComponent<RocketMovement>().damage = 10;   
+            rocketInstance.GetComponent<RocketMovement>().speed = 10;   
+            onShootRocket.Invoke();
+        }
+        shooting = false;
+        shootTimer += Time.deltaTime;
+    }
+
+    private void Reload()
+    {
+       // if (reloading && !shooting && !reloadingAnimation)
+       // {
+          //  if (currentMagazine < maxMagazine)
+         //   {
+          //      onReloadRocket.Invoke();
+          //      reloadingAnimation = true;
+                
+         //   }
+         //   else
+         //   {
+         //       reloading = false;
+           // }
+       // }
+       
+       if (reloading)
+       {
+           if (currentMagazine < maxMagazine)
+           {
+               currentMagazine = maxMagazine;
+               magazineText.GetComponent<Text>().text = $"{currentMagazine}/{maxMagazine}";
+               onReloadRocket.Invoke(); 
+           }
+
+           reloading = false;
+       }
+    }
+
+    private void EndReload()
+    {
+        currentMagazine = maxMagazine;
+        magazineText.GetComponent<Text>().text = $"{currentMagazine}/{maxMagazine}";
+        reloading = false;
+        //reloadingAnimation = false;
+    }
+}
+
---
+++ Assets/Scripts/Weapons/RocketMovement.cs
@@ -0,0 +1,109 @@
+
+using UnityEngine;
+
+public class RocketMovement : MonoBehaviour
+{
+    [SerializeField] private float explosionRadius = 5f;
+    [SerializeField] private LayerMask damageLayer;
+          
+    [SerializeField] public float damage;                   
+    [SerializeField] private GameObject breakEffect;         
+    [SerializeField] private GameObject rocketObjects;       
+    
+    private Rigidbody rb;
+    private float despawnTimer = 0f;                         
+    private bool toDestroy = false;                          
+    private float destroyTimer = 0f;
+    [SerializeField] public float speed = 10f;
+    public Vector3 directionSet { private get; set; } = Vector3.zero;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        
+       
+        if (directionSet == Vector3.zero)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+            RaycastHit hit;
+
+            Vector3 targetPoint;
+            if (Physics.Raycast(ray, out hit))
+            {
+                targetPoint = hit.point; 
+            }
+            else
+            {
+                targetPoint = ray.origin + ray.direction * 200;
+            }
+
+            Vector3 direction = targetPoint - transform.position;
+            rb.AddForce(direction.normalized * speed, ForceMode.Impulse);
+        }
+        else
+        {
+            rb.AddForce(directionSet.normalized * speed, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag != "Player" && other.gameObject.tag != "Weapon" && other.gameObject.tag != "Bullets")
+        {
+            rb.velocity = Vector3.zero;
+            rocketObjects.SetActive(false);         
+            GetComponent<CapsuleCollider>().enabled = false; 
+            
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, explosionRadius, damageLayer);
+            foreach (Collider hit in hitColliders)
+            {
+                if (hit.CompareTag("Enemy"))
+                {
+                    hit.GetComponent<EnemyHealthComponent>()?.DealDamage(damage, transform.position);
+                }
+            }
+            
+            Instantiate(breakEffect, transform.position, Quaternion.identity);
+
+            toDestroy = true;  
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+
+    void Update()
+    {
+        RocketDespawn();
+        toDestroyRocket();
+    }
+
+    private void RocketDespawn()
+    {
+        if (!toDestroy)
+        {
+            
+            if (despawnTimer >= 10f)
+            {
+                Destroy(gameObject);
+            }
+            despawnTimer += Time.deltaTime;
+        }
+    }
+
+    private void toDestroyRocket()
+    {
+        if (toDestroy)
+        {
+            if (destroyTimer >= 0.6f)
+            {
+                Destroy(gameObject);
+            }
+            destroyTimer += Time.deltaTime;
+        }       
+    }
+}
