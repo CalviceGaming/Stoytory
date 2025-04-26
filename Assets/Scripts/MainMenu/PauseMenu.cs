7cdd67d469110f6d55efdbcbbe7779b50bd7daec
Tomás Monteiro <152517065+tomasotaldomonteiro@users.noreply.github.com>
2025-04-26 21:53:17 +0100

    tenta meter a funcior

---
+++ Assets/Scripts/MainMenu/PauseMenu.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+    private bool isPaused = false;
+    
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    private void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void BackToMenu()
+    {
+        Time.timeScale = 1f; 
+        Debug.Log("Trying to load MainMenu...");
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
